683ce0dc7be89b7a5a185852cfbe1d14699333d5
Arnd Bergmann <arnd@arndb.de>
2018-04-27 15:40:16 +0200

    timekeeping: Add more coarse clocktai/boottime interfaces
    The set of APIs we provide has a few holes for coarse times, e.g. we
    provide ktime_get_coarse_boottime() and ktime_get_boottime_ts64(),
    but not the combination of the two.
    
    This adds four new functions:
    
    ktime_get_coarse_boottime_ts64()
    ktime_get_boottime_seconds()
    ktime_get_coarse_clocktai_ts64()
    ktime_get_clocktai_seconds()
    
    to fill in some of the missing pieces. I have missed only the
    ktime_get_boottime_seconds() accessor in a few occasions in
    the past, but it seems better to just provide all four together,
    as there is very little cost to having them.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Cc: y2038@lists.linaro.org
    Cc: John Stultz <john.stultz@linaro.org>
    Link: https://lkml.kernel.org/r/20180427134016.2525989-6-arnd@arndb.de

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -752,11 +752,6 @@ static inline void set_to_next_sit(struct sit_info *sit_i, unsigned int start)
 #endif
 }
 
-static inline time64_t ktime_get_boottime_seconds(void)
-{
-	return ktime_divns(ktime_get_boottime(), NSEC_PER_SEC);
-}
-
 static inline unsigned long long get_mtime(struct f2fs_sb_info *sbi,
 						bool base_time)
 {
