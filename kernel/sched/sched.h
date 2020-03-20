c65a7ec96b62a16b5c1f5a09bb768eb2ac3986e0
Vincent Donnefort <vincent.donnefort@arm.com>
2020-03-20 13:21:35 +0000

    sched/core: Remove unused rq::last_load_update_tick
    The following commit:
    
      5e83eafbfd3b ("sched/fair: Remove the rq->cpu_load[] update code")
    
    eliminated the last use case for rq->last_load_update_tick, so remove
    the field as well.
    
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lkml.kernel.org/r/1584710495-308969-1-git-send-email-vincent.donnefort@arm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1029,8 +1029,7 @@ struct rq {
 	unsigned long cpu_load[CPU_LOAD_IDX_MAX];
 #ifdef CONFIG_NO_HZ_COMMON
 #ifdef CONFIG_SMP
-	unsigned long last_load_update_tick;
-	unsigned long last_blocked_load_update_tick;
+	unsigned long		last_blocked_load_update_tick;
 	unsigned int		has_blocked_load;
 	call_single_data_t	nohz_csd;
 #endif /* CONFIG_SMP */
