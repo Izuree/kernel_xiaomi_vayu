0e08957c66744aa167a7042b01b9551c4ef5a5e1
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-07-16 19:53:36 +0700

    Revert "block: zram: increase hardcoded zram block size to 6gb"
    This reverts commit 8db26a800cea002302f43047de6762618e7230b6.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1737,7 +1737,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = PAGE_ALIGN((u64)SZ_1G * 6);
+	disksize = PAGE_ALIGN((u64)SZ_4G);
 	pr_info("Setting zRAM size to %li GB", disksize / 1073741824);
 
 	down_write(&zram->init_lock);
